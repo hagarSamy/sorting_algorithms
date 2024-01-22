6e5d683beab0849aa95ec4442f8b17f400291cea
HagarSamy <hagarsami63@gmai.com>
2024-01-22 22:55:31 +0200

    Bubble sorting

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,14 +3,15 @@
 
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j, k;
+    size_t i, k;
     int temp;
     bool swapped;
 
+    if (size <= 1)
+		return;
     for(k = 0; k < size - 1; k++)
     {
         swapped = false;
-    
         for(i = 0; i < size - k - 1; i++)
         {
             if (array[i] >= array[i + 1])
@@ -19,15 +20,8 @@ void bubble_sort(int *array, size_t size)
                 array[i] = array[i + 1];
                 array[i + 1] = temp;
                 swapped = true;
-                for(j = 0; j < size; j++)
-                {
-                    printf("%d", array[j]);
-                    if (j != size - 1)
-                    {
-                        printf(", ");
-                    }
-                }
-                printf("\n");
+                print_array(array, size);
+                swapped = true;
             }
         }
         if (!swapped)
