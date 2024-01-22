0775da8dda303161fbbf8ff95bfead8aafcb8017
hagarSamy <hs1800@fayoum.edu.eg>
2024-01-23 00:04:36 +0200

    Bubble sorting

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,35 +1,36 @@
 #include "sort.h"
-#include "stdbool.h"
-/**
- * bubble_sort - sorts an array
- * @array: a ponter to the array to sort
- * @size: the size of the array
- * Return: nothing
-*/
 
+/**
+ * bubble_sort - func using bubble sort algo to sort array
+ *
+ * @array: array integers
+ * @size: size of array.
+*/
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, k;
-	int temp;
-	bool swapped;
+	size_t x = 0, y;
+	int sp;
 
-	if (!array || size <= 1)
-		return;
-	for (k = 0; k < size - 1; k++)
+	if (!size || !array)
 	{
-		swapped = false;
-		for (i = 0; i < size - k - 1; i++)
+		return;
+	}
+
+	while (x < size)
+	{
+		for (y = 0; y < size - 1; y++)
 		{
-			if (array[i] > array[i + 1])
+			if (array[y] > array[y + 1])
 			{
-				temp = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = temp;
+				sp = array[y];
+				array[y] = array[y + 1];
+				array[y + 1] = sp;
+
 				print_array(array, size);
-				swapped = true;
 			}
 		}
-		if (swapped == false)
-			break;
+
+		x++;
 	}
+
 }
