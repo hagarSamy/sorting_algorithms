54d754eaaeaebe5d010feb67854f923d93dd03db
hagarSamy <hs1800@fayoum.edu.eg>
2024-01-25 01:53:28 +0200

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,22 +9,25 @@
 
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j;
+	size_t i, j, min;
 	int temp;
 
 	if (!size || !array)
 		return;
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
+		min = i;
 		for (j = 1 + i; j < size; j++)
 		{
-			if (array[i] > array[j])
-			{
-				temp = array[i];
-				array[i] = array[j];
-				array[j] = temp;
-				print_array((const int *)array, size);
-			}
+			if (array[j] < array[min])
+				min = j;
+		}
+		if (min != i)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array((const int *)array, size);
 		}
 	}
 }
