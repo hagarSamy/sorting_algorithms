67f691bfb7e1b1ad2b9d189cd98349ca6587ca5d
HagarSamy <hagarsami63@gmai.com>
2024-01-24 18:19:30 +0200

    selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * election_sort - sorts aan array according to selection sort
+ * @array: a pointer to the array
+ * Return: void
+*/
+
+void selection_sort(int *array, size_t size)
+{
+    size_t i, j;
+    int temp;
+
+    if (!size || !array)
+        return;
+    for(i = 0; i < size; i++)
+    {
+        for (j = 0 + i; j < size; j++)
+        {
+            if (array[i] > array[j])
+            {
+                temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                print_array((const int *)array, size);
+            }
+        }
+    }
+}
