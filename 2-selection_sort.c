150ebd9e11aca7c5cce5e23a53bab0b4c16c6321
hagarSamy <hs1800@fayoum.edu.eg>
2024-01-25 01:58:14 +0200

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,19 +1,23 @@
 #include "sort.h"
+
 /**
- * selection_sort - sort an array using the selection algorithm
- * @array: Array to sort
- * @size: size of the array
- */
+ * selection_sort - sorts aan array according to selection sort
+ * @array: a pointer to the array
+ * @size: length of the array
+ * Return: void
+*/
 
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min;
 	int temp;
 
+	if (!size || !array)
+		return;
 	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
-		for (j = i + 1; j < size; j++)
+		for (j = 1 + i; j < size; j++)
 		{
 			if (array[j] < array[min])
 				min = j;
